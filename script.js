e2712b1c6c6592f2193ab9578b5f78f089b593c8
Alphonse Kusluch <alkusluch@gmail.com>
2023-10-21 12:10:56 -0400

    fix the color function in javascript and add a local storage data recovery function

--- script.js
+++ script.js
@@ -1,57 +1,45 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// on load hande color coding and data recover yfrom local storage
 $(document).ready(function() {
   var date = dayjs().format('dddd MMMM D')
   var hour =dayjs().format('H')
 
   $('#currentDay').text(date);
-
+  // color code based off time of day
   function color(){
     $('.time-block').each(function(){
 
-     if (parseInt($('.time-block').attr("id")) < hour) {
-      $('.time-block').addClass("past")
-     }
-     
-     if (parseInt($('.time-block').attr("id")) === hour) {
-      $('.time-block').addClass("present")
+     if (parseInt($(this).attr("id")) < hour) {
+      $(this).addClass("past")
      }
 
-     if (parseInt($('.time-block').attr("id")) > hour) {
-      $('.time-block').addClass("future")
+     if (parseInt($(this).attr("id")) == hour) {
+      $(this).addClass("present")
+     }
+
+     if (parseInt($(this).attr("id")) > hour) {
+      $(this).addClass("future")
      }
     })
   }
-  color()
+  // call color function
+  color();
+  // handle the data recovery from local storage
+  function handleRecovery(){
+    $('.time-block').each(function() {
+      var content = localStorage.getItem($(this).attr("id"));
+      if (content !== null) {
+        this.children[1].textContent = content
+      }
+    })
+  }
+  handleRecovery();
 });
-
+// function for handling save content
 function handleSave(event){
   console.log(event.target)
   var activity = event.target.parentElement.parentElement.getAttribute('id');
   var content = event.target.parentElement.parentElement.children[1].value;
   localStorage.setItem(activity, content);
 }
-
-// window.onload = onLoad();
-
+// event liscener for save handling
 $('.time-block').on('click', '.saveBtn', handleSave);
-
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
