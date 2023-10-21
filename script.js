1194a65ea5da35fc41affffe2f7d7377c6f667c6
Alphonse Kusluch <alkusluch@gmail.com>
2023-10-20 20:59:25 -0400

    add variable for curent date to be displayed and a onload function to attemp the add classes to the timeblocks as well as get local storage

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var date = dayjs().format('dddd MMMM D')
+$('#currentDay').text(date);
+
+var hour =dayjs().format('H')
+var timeBlock = $('.time-block')
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +28,36 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+function onLoad() {
+  for (i=0; i<timeBlock ; i++) {
+    var saved = localStorage.getItem(parseInt(timeBlock[i].getAttribute("id")));
+    timeBlock[i].children[1].textContent = saved ;
+
+    if (parseInt(timeBlock[i].getAttribute("id")) < hour ) {
+      timeBlock[i].addClass("past");
+    }
+
+    if (parseInt(timeBlock[i].getAttribute("id")) === hour ) {
+      timeBlock[i].addClass("present");
+    }
+
+    if (parseInt(timeBlock[i].getAttribute("id")) > hour ) {
+      timeBlock[i].addClass("future");
+    }
+  }
+}
+
+
+
+function handleSave(event){
+  console.log(event.target)
+  var activity = event.target.parentElement.parentElement.getAttribute('id');
+  var content = event.target.parentElement.parentElement.children[1].value;
+  localStorage.setItem(activity, content);
+}
+
+window.onload = onLoad();
+
+$('.time-block').on('click', '.saveBtn', handleSave);
