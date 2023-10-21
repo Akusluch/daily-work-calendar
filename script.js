e5212627f41601e72eb40e257e7dec02844ac9c9
Alphonse Kusluch <alkusluch@gmail.com>
2023-10-20 22:07:28 -0400

    replace onload function with jquery equivilent

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function() {
+  var date = dayjs().format('dddd MMMM D')
+  var hour =dayjs().format('H')
 
-var date = dayjs().format('dddd MMMM D')
-$('#currentDay').text(date);
+  $('#currentDay').text(date);
 
-var hour =dayjs().format('H')
-var timeBlock = $('.time-block')
+  function color(){
+    $('.time-block').each(function(){
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+     if (parseInt($('.time-block').attr("id")) < hour) {
+      $('.time-block').addClass("past")
+     }
+     
+     if (parseInt($('.time-block').attr("id")) === hour) {
+      $('.time-block').addClass("present")
+     }
+
+     if (parseInt($('.time-block').attr("id")) > hour) {
+      $('.time-block').addClass("future")
+     }
+    })
+  }
+  color()
+});
+
+function handleSave(event){
+  console.log(event.target)
+  var activity = event.target.parentElement.parentElement.getAttribute('id');
+  var content = event.target.parentElement.parentElement.children[1].value;
+  localStorage.setItem(activity, content);
+}
+
+// window.onload = onLoad();
+
+$('.time-block').on('click', '.saveBtn', handleSave);
+
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -27,37 +55,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
-
-
-function onLoad() {
-  for (i=0; i<timeBlock ; i++) {
-    var saved = localStorage.getItem(parseInt(timeBlock[i].getAttribute("id")));
-    timeBlock[i].children[1].textContent = saved ;
-
-    if (parseInt(timeBlock[i].getAttribute("id")) < hour ) {
-      timeBlock[i].addClass("past");
-    }
-
-    if (parseInt(timeBlock[i].getAttribute("id")) === hour ) {
-      timeBlock[i].addClass("present");
-    }
-
-    if (parseInt(timeBlock[i].getAttribute("id")) > hour ) {
-      timeBlock[i].addClass("future");
-    }
-  }
-}
-
-
-
-function handleSave(event){
-  console.log(event.target)
-  var activity = event.target.parentElement.parentElement.getAttribute('id');
-  var content = event.target.parentElement.parentElement.children[1].value;
-  localStorage.setItem(activity, content);
-}
-
-window.onload = onLoad();
-
-$('.time-block').on('click', '.saveBtn', handleSave);
